ba330f8b7e93d78ea857148b0b14378b8d82c944
Dale-Kurt Murray <hello@dalekurtmurray.com>
2024-07-05 15:31:34 -0400

    Update Render configuration to use REDIS_URL

--- stock_fight/app/config.py
+++ stock_fight/app/config.py
@@ -1,12 +1,12 @@
 # path: stock_fight/app/config.py
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, Field
 
 
 class Settings(BaseSettings):
-    finnhub_api_key: str
-    alpha_vantage_api_key: str
-    redis_url: str
+    finnhub_api_key: str = Field(..., env="FINNHUB_API_KEY")
+    alpha_vantage_api_key: str = Field(..., env="ALPHA_VANTAGE_API_KEY")
+    redis_url: str = Field(..., env="REDIS_URL")
     redis_ttl: int = 3600  # default to 1 hour
 
     class Config:

8ed8f8e6534466b014ae9e38c2133d5089c0a4ea
Dale-Kurt Murray <hello@dalekurtmurray.com>
2024-07-05 15:50:25 -0400

    Update Render configuration to use VITE_BACKEND_URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,12 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:8000',
+        target: process.env.VITE_BACKEND_URL || 'http://localhost:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
